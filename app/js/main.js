31e31807d085e15ba8734f5e2042f90289898bca
Alexander Grobovsky <ktotaam@mail.ru>
2020-05-21 12:38:26 +0500

    added featured-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function () {
 
+    $('.rate-star').rateYo({
+        rating: 4.5,
+        readOnly: true,
+        starWidth: "15px",
+        normalFill: "#ccc"
+    });
+
+    $('.featured__slider').slick({
+
+    })
+
 });
