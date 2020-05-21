8cf2fa25ca2a562e6d64e050f944a6db298eed3c
Alexander Grobovsky <ktotaam@mail.ru>
2020-05-21 23:45:08 +0500

    finished with featured-slider

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,10 @@ $(function () {
     });
 
     $('.featured__slider').slick({
-
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/slider-left-arrow.svg" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/slider-right-arrow.svg" alt=""></button>',
+        autoplay: true,
+        autoplaySpeed: 3000,
     })
 
 });
