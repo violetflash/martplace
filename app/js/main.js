d24baf11a39b266e2d6dbdfd73b8da0b1ee8772a
Alexander Grobovsky <ktotaam@mail.ru>
2020-05-22 23:58:23 +0500

    done with the new products section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,39 @@
 $(function () {
 
+    var mixer = mixitup('.products__inner-box');
+
+
+    $('button.products__filter-btn').click(function(){
+        if ( $('.products__menu-box').css('visibility') == 'hidden' )
+            $('.products__menu-box').css('visibility','visible');
+        myDropDown = $(this).next('.products__menu-box');
+
+        if( myDropDown.is(':visible') ) {
+            $(this).removeClass('drop-down-open');
+            myDropDown.hide();
+        } else {
+            myDropDown.fadeIn();
+            $(this).addClass('drop-down-open');
+        }
+
+        return false;
+    });
+
+    $('html').click(function(e) {
+        $('.products__menu-box').hide();
+    });
+
+    $('.products__menu-box').click(function(e){
+        e.stopPropagation();
+    });
+
+
+
+
+
+
+
+
     $('.featured-stars').rateYo({
         rating: 4.5,
         readOnly: true,
@@ -22,4 +56,10 @@ $(function () {
         autoplaySpeed: 3000,
     })
 
+});
+
+jQuery(document).ready(function() {
+    jQuery('.products__filter-btn').click(function() {
+        jQuery('.products__menu-box').toggleClass('visible');
+    });
 });
