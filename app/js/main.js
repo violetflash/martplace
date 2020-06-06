49023bba3639996acb53f3e356f00aa10afe7e89
Alexander Grobovsky <ktotaam@mail.ru>
2020-06-06 18:58:28 +0500

    1st page complete

--- app/js/main.js
+++ app/js/main.js
@@ -20,7 +20,9 @@ $(function () {
         prevArrow: '<button class="featured__slick-arrow featured__slick-arrow--prev"><span class="lnr lnr-chevron-left"></span></button>',
         nextArrow: '<button class="featured__slick-arrow featured__slick-arrow--next"><span class="lnr lnr-chevron-right"></span></button>',
         appendArrows: '.featured__slider-nav',
-        // autoplay: true,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        autoplay: true,
         autoplaySpeed: 3000,
         adaptiveHeight: true,
         responsive: [
@@ -92,7 +94,6 @@ $(function () {
         from: 30,
         to: 300,
         prefix: "$"
-        // grid: true
     });
 
     try {
